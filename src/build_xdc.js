65c9e50e668d4ee41fcbbf3543502bfd273bd931
adbenitez <asieldbenitez@gmail.com>
2024-10-13 18:12:08 +0200

    update deps, set default file filter for buildXDC()

--- src/build_xdc.js
+++ src/build_xdc.js
@@ -1,7 +1,11 @@
 const zipPack = require("vite-plugin-zip-pack").default;
 
+function defaultFilter(fileName, filePath, isDirectory) {
+  return !fileName.endsWith("~");
+}
+
 // package your app as .xdc file on build
 exports.buildXDC = function (opts = {}) {
-  opts = Object.assign({ outDir: "dist-xdc", outFileName: "app.xdc" }, opts);
+  opts = Object.assign({ outDir: "dist-xdc", outFileName: "app.xdc", filter: defaultFilter }, opts);
   return zipPack(opts);
 };
